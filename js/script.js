1972012c39cd54198fea9c09fbc8558ff2b2752c
webakira <webakira@ya.ru>
2021-10-26 17:44:24 +0200

    tests

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// TODO Хлебные крошки в виде слайдера
+function initBreadcrumbsSlider() {
+	let bradCrumbsSlider = new Swiper('.breadcrumbs-slider__area', {
+		autoplay: false,
+		slidesPerView: 'auto',
+		//width: 'auto',
+		grabCursor: true,
+		speed: 800,
+		navigation: false,
+		pagination: false,
+		initialSlide: countSlides(),
+		watchOverflow: true,
+	});
+	function countSlides() {
+		let slides, countSlides;
+		slides = document.querySelectorAll('.breadcrumbs-slider__item');
+		countSlides = slides.length
+		return countSlides;
+	}
+}
+// ? END Хлебные крошки в виде слайдера
+
+initBreadcrumbsSlider();
